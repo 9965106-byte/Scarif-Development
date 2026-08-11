f00d222c38d483156b2285d605068a23043decdf
9965106-byte <9965106@schoolsnet.act.edu.au>
2026-08-11 15:24:26 +1000

    iot

---
+++ IOT/src/main.cpp
@@ -0,0 +1,68 @@
+// MQTT client name
+// TODO - Change the name to the specific module name.
+const char *mqttClient = "ESP32-Musa"; // This should be unique for each ESP32, e.g: "ESP32_Servo", "ESP32_Piezo", etc
+
+// MQTT Topic
+const char *mqttTopic;
+
+#include <Arduino.h>
+#include "comms.h"
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+void performActionBasedOnPayload(String payload)
+{
+    Serial.print("Payload: ");
+    Serial.println(payload);
+    if ((char)payload[0] == '1')
+    {
+        Serial.println("LED ON");
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    wifiSetup();
+    mqttSetup();
+    while (!Serial)
+    {
+        delay(10);
+    }
+    delay(1000);
+
+    randomSeed(analogRead(A0)); // Seed using an unconnected analog pin for real randomness
+
+    // Make sure the sensor is found, you can also pass in a different i2c
+    // address with tempsensor.begin(0x49) for example
+    if (!tempsensor.begin())
+    {
+        Serial.println("Couldn't find ADT7410!");
+        while (1) ;
+    }
+}
+
+void loop()
+{
+    // 1. Handle Connection Persistence
+    mqttConnect(); // Ensure we are connected to the MQTT broker. If not, this will attempt to reconnect.
+
+    // 2. Generate and send a random number periodically
+    int randomNumber = random(1, 100001);
+
+     float tempinC = tempsensor.readTempC();
+     Serial.print("Temp: "); 
+    sendPeriodicUpdate("sensorData", String(tempinC));
+
+    client.loop(); // Check for incoming messages and keep the connection alive
+    delay(100);
+}
